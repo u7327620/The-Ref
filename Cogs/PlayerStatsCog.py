4aa99a9c3c9a1acda4ef45668273840157633a92
u7327620 <u7327620@anu.edu.au>
2025-07-27 12:26:45 +1000

    Ephemeral only

--- Cogs/PlayerStatsCog.py
+++ Cogs/PlayerStatsCog.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import app_commands, Interaction
 import requests
 
 
@@ -6,15 +7,15 @@ class PlayerStatsCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="get_stats", description="Retrieves TFC fighter profile")
-    async def get_stats(self, ctx: commands.Context, name: str):
+    @app_commands.command(name="get_stats", description="Retrieves TFC fighter profile")
+    async def get_stats(self, ctx: Interaction, name: str):
         response = requests.get(f"https://github.com/u7327620/TMA_Records/raw/refs/heads/main/Data/Fighters/{name.lower()}_Database.txt")
         if response.status_code == 404:
-            return await ctx.send(f"No TFC fighter \"{name}\" found", ephemeral=True)
+            return await ctx.response.send_message(f"No TFC fighter \"{name}\" found", ephemeral=True)
         elif response.status_code != 200:
-            return await ctx.send(f"error {response.status_code} looking up {name}", ephemeral=True)
+            return await ctx.response.send_message(f"error {response.status_code} looking up {name}", ephemeral=True)
         else:
-            return await ctx.send(response.text, ephemeral=True)
+            return await ctx.response.send_message(response.text, ephemeral=True)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(PlayerStatsCog(bot))
